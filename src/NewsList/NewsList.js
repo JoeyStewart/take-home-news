4273a5179a7ee6540709168ac8085847a445b5ac
Joseph Stewart <JosephStewart1752@gmail.com>
2024-01-27 17:44:08 -0500

    Add elements to card

--- src/NewsList/NewsList.js
+++ src/NewsList/NewsList.js
@@ -1,38 +1,29 @@
-import Card from "../Card/Card.js"
-import './Movies.css'
-import PropTypes from 'prop-types'
+import Card from "../Card/Card.js";
+import './NewsList.css'
+import PropTypes from 'prop-types';
 
-export default function Movies({ movies, showDetails }) {
-  if (!movies || movies.length === 0) {
+export default function NewsList({ newsArticles, showDetails }) {
+  if (!newsArticles || newsArticles.length === 0) {
     return null;
   } else {
     return (
-      <div className="movies-container">
-      {movies.map((movie) => (
-        <Card 
-        title={movie.title} 
-        image={movie.poster_path} 
-        rating={movie.average_rating.toFixed(1)}
-        id={movie.id}
-        key={movie.id} 
-        showDetails={() => showDetails(movie.id)}
-        />
+      <div className="news-container">
+        {newsArticles.map((newsArticle) => (
+          <Card 
+            id={newsArticle.id}
+            author={newsArticle.author}
+            title={newsArticle.title} 
+            description={newsArticle.description} 
+            content={newsArticle.content}
+            image={newsArticle.urlToImage}
+            url={newsArticle.url}
+            showDetails={() => showDetails(newsArticle.id)}
+          />
         ))}
-    </div>
-  );
+      </div>
+    );
+  }
 }
-}
 
 
-Movies.propTypes = {
-  movies: PropTypes.arrayOf(
-   PropTypes.shape({
-     title: PropTypes.string,
-     poster_path: PropTypes.string,
-     average_rating: PropTypes.number,
-     id: PropTypes.number,
-   })
-  ).isRequired,
-  showDetails: PropTypes.func.isRequired,
-
-};
+//Add propTypes
