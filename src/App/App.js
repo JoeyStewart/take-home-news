7be142ca039245218f26f095ebc5e179d58d0051
Joseph Stewart <JosephStewart1752@gmail.com>
2024-01-27 17:43:25 -0500

    fetch call and display results

--- src/App/App.js
+++ src/App/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+
+import NewsList from '../NewsList/NewsList.js';
 import './App.css';
-Test
+// import NewsDetails from '../NewsDetails/NewsDetails.js';
+
 function App() {
+  const [newsArticles, setNewsArticles] = useState([]);
+
+  const fetchNewsArticles = () => {
+    fetch('https://newsapi.org/v2/everything?domains=wsj.com&apiKey=676dc2532ad3454b8903fce9bd2bc8a5')
+      .then(response => response.json())
+      .then(data => setNewsArticles(data.articles))
+      .catch(error => console.error('Error fetching news articles:', error));
+  };
+
+  useEffect(() => {
+    fetchNewsArticles();
+  }, []);
+
+  const backToMain = () => {
+    document.body.style.overflow = 'visible';
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <main className='App'>
+      <header>
+        <h1 className='news-header' style={{ cursor: 'pointer' }} onClick={backToMain}>
+          News App
+        </h1>
       </header>
-    </div>
+      <NewsList newsArticles={newsArticles} />
+    </main>
   );
 }
 
