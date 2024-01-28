e1697ad7b411e152c9c3e7cf08385130dfe8ed36
Joseph Stewart <JosephStewart1752@gmail.com>
2024-01-27 19:51:58 -0500

    Add cards to display

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -2,25 +2,27 @@ import './Card.css';
 import { useInView } from 'react-intersection-observer';
 import PropTypes from 'prop-types';
 
-export default function Card({ title, image, showDetails, description, author, content, url, id }) {
+export default function Card({ title, image, date, showDetails, description, author, content, url}) {
     const [ref, inView] = useInView({
       triggerOnce: true, 
       threshold: 0.2, 
     });
-  
+
     return (
-      <main to={`/${id}`} className='card-link'>
+      <main to={`/${title}`} className='card-link'>
         <section
           ref={ref}
           style={{ cursor: 'pointer' }}
           className={`card ${inView ? 'fade-in' : ''}`}
+          onClick={() => showDetails(title)}
         >
           <div className='news-info'>
             <h3 className='title'>{title}</h3>
             {image && <img className='image' src={image} alt={title} />}
-            <h3 className='author'>{author}</h3>
+            <h3 className='author'>Author: {author}</h3>
             <p className='description'>{description}</p>
             <p className='content'>{content}</p>
+            <p className='date'>{date}</p>
             <p className='url'>{url}</p>
           </div>
         </section>
@@ -30,9 +32,9 @@ export default function Card({ title, image, showDetails, description, author, c
 
 Card.propTypes = {
   title: PropTypes.string.isRequired,
-  image: PropTypes.string.isRequired,
+  image: PropTypes.string,
   showDetails: PropTypes.func.isRequired,
-  description: PropTypes.string.isRequired,
+  description: PropTypes.string,
   author: PropTypes.string.isRequired,
   content: PropTypes.string.isRequired,
   url: PropTypes.string.isRequired,
