2caff48600b71faaa7ca5a02d6cb187c573ca923
Joseph Stewart <JosephStewart1752@gmail.com>
2024-01-28 14:41:52 -0500

    removed unwanted code

--- src/NewsDetails/NewsDetails.js
+++ src/NewsDetails/NewsDetails.js
@@ -6,16 +6,19 @@ export default function NewsDetails({ singleArticle }) {
   if (!singleArticle) {
     return null;
   }
+
   const rawDate = new Date(singleArticle.publishedAt);
   const formattedDate = `${rawDate.getFullYear()}-${String(rawDate.getMonth() + 1).padStart(2, '0')}-${String(rawDate.getDate()).padStart(2, '0')}`;
+
   return (
     <div className="news-details-container">
       <div className="news-details">
-        <h2>{singleArticle.title}</h2>
-        <p>{singleArticle.description}</p>
-        <p>{singleArticle.content}</p>
-        <a href={singleArticle.url} target="_blank" rel="noopener noreferrer">Click to Read the Full Article!</a>
-        <p>{formattedDate}</p>
+        <h2 className="single-title">{singleArticle.title}</h2>
+        {singleArticle.urlToImage && <img className='image' src={singleArticle.urlToImage} alt={singleArticle.title} />}
+        <h3 className="single-author">Author: {singleArticle.author}</h3> 
+        <p className="single-date">{formattedDate}</p>
+        <p className="single-content">{singleArticle.content}</p>
+        <a className="single-url" href={singleArticle.url} target="_blank" rel="noopener noreferrer">Click to Read the Full Article!</a>
       </div>
     </div>
   );
